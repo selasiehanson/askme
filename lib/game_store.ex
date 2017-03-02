5ee22d162f86dfdec60b26a0aaaa6b9a27227422
selasie hanson <soundfever18@gmail.com>
2017-03-02 23:57:35 +0000

    Adds game library which lists all available games

--- lib/game_store.ex
+++ lib/game_store.ex
@@ -1,18 +1,20 @@
-defmodule Askme.GameLibrary do  use GenServer
+defmodule Askme.GameLibrary do  
+  use GenServer
 
   def start_link do 
+    store = %{}
+    games = Askme.GameLoader.all_games
+            |> Enum.into(%{}, fn game -> {game.title, game } end)
 
-  end
-
-  def load_games do 
-
+    {:ok, pid } = GenServer.start_link(__MODULE__, games, name: __MODULE__)
   end
 
   def list_games  do  
-
+    GenServer.call __MODULE__, :list_games
   end
 
   def handle_call(:list_games, _from, state) do
-
+    games = Map.keys state
+    {:reply, games, state} 
   end
 end
